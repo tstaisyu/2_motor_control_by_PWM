b51913934959080be6937acc242cb06bca4ed2ab
Taisyu Shibata <t_shiba117@yahoo.co.jp>
2023-12-28 10:36:20 +0900

    Add JoyStick Control

--- 2_motor_control_by_PWM.ino
+++ 2_motor_control_by_PWM.ino
@@ -1,38 +1,35 @@
 #include <M5Stack.h>
+#include "CytronMotorDriver.h"
 
 // Define GPIO pins for motor control
-const int motor1Pin1 = 33; // Example: Motor 1 Pin 1
-const int motor1Pin2 = 32; // Example: Motor 1 Pin 2
-const int motor2Pin1 = 25; // Example: Motor 2 Pin 1
-const int motor2Pin2 = 26; // Example: Motor 2 Pin 2
+const int motorRPin1 = 25; // Example: Motor 1 Pin 1
+const int motorRPin2 = 18; // Example: Motor 1 Pin 2
+const int motorLPin1 = 26; // Example: Motor 2 Pin 1
+const int motorLPin2 = 19; // Example: Motor 2 Pin 2
+
+CytronMD motor_R(PWM_DIR, motorRPin1, motorRPin2);
+CytronMD motor_L(PWM_DIR, motorLPin1, motorLPin2);
 
 void setup() {
   // Initialize M5Stack
   M5.begin();
-
-  // Set motor control pins as output
-  pinMode(motor1Pin1, OUTPUT);
-  pinMode(motor1Pin2, OUTPUT);
-  pinMode(motor2Pin1, OUTPUT);
-  pinMode(motor2Pin2, OUTPUT);
+  M5.Lcd.setTextSize(2);
 }
 
 void loop() {
-  // Rotate motor 1 in one direction
-  analogWrite(motor1Pin1, 200); // Set PWM value to 200
-  analogWrite(motor1Pin2, 0);   // Set PWM value to 0
-
-  // Rotate motor 2 in the opposite direction
-  analogWrite(motor2Pin1, 0);   // Set PWM value to 0
-  analogWrite(motor2Pin2, 200); // Set PWM value to 200
-
-  delay(2000); // Delay for 2 seconds
-
-  // Stop the motors
-  analogWrite(motor1Pin1, 0);
-  analogWrite(motor1Pin2, 0);
-  analogWrite(motor2Pin1, 0);
-  analogWrite(motor2Pin2, 0);
-
-  delay(2000); // Delay for 2 seconds
+  int16_t speed1 = map(analogRead(35), 0, 4095, -255, 255);
+  int16_t speed2 = map(analogRead(36), 0, 4095, -255, 255);
+ 
+  M5.Lcd.setCursor(0, 0);
+  M5.Lcd.print("R: ");
+  M5.Lcd.print(speed1, DEC);
+  M5.Lcd.println("      ");
+  M5.Lcd.print("L: ");
+  M5.Lcd.print(speed2, DEC);
+  M5.Lcd.println("      ");
+  
+  motor_R.setSpeed(speed1);
+  motor_L.setSpeed(speed2);
+  delay(20);
+  
 }
